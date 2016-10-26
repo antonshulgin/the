c9eab8c45b40bd3b3f9c394b7bb466de546ee8d9
Anton Shulgin <anton.shulgin@gmx.com>
2016-10-26 15:32:35 +0300

    structural changes to allow delayed checks after initialisation

--- src/the.js
+++ src/the.js
@@ -3,135 +3,145 @@
 
 	var TAG = '[the.js]';
 
-	var internals = {};
-	var externals = {};
-
 	function the(value) {
-		internals.reset();
-		internals.setValue(value);
+		var internals = {};
+		var externals = {};
+
+		setValue(value);
+
+		externals.isNumber = isNumber;
+		externals.isDefined = isDefined;
+		externals.isNull = isNull;
+		externals.isHexString = isHexString;
+		externals.isString = isString;
+		externals.isBoolean = isBoolean;
+		externals.isFunction = isFunction;
+		externals.isObject = isObject;
+		externals.isArray = isArray;
+		externals.isDate = isDate;
+		externals.isRegExp = isRegExp;
+		externals.isInstanceOf = isInstanceOf;
+		externals.hasPrototypeOf = hasPrototypeOf;
+
 		return externals;
-	}
 
-	externals.hasPrototypeOf = function (constructor, isSilent) {
-		var value = internals.getValue();
-		var theValue = the(value);
-		var isDefined = theValue.isDefined(true);
-		var isNotNull = !theValue.isNull(true);
-		var hasPrototypeOf = isDefined && isNotNull && Object.getPrototypeOf(value).constructor.name === constructor.name;
-		return internals.out(hasPrototypeOf, 'value to have the prototype of ' + constructor.name, isSilent);
-	};
-
-	externals.isInstanceOf = function (constructor, isSilent) {
-		var value = internals.getValue();
-		var hasPrototypeOf = the(value).hasPrototypeOf(constructor, true);
-		var isInstanceOf = hasPrototypeOf && (value instanceof constructor);
-		return internals.out(isInstanceOf, 'an instance of ' + constructor.name, isSilent);
-	};
-
-	externals.isRegExp = function (isSilent) {
-		var value = internals.getValue();
-		var isRegExp = (Object.prototype.toString.call(value) === '[object RegExp]');
-		return internals.out(isRegExp, 'a regular expression', isSilent);
-	};
-
-	externals.isDate = function (isSilent) {
-		var value = internals.getValue();
-		var isDate = (Object.prototype.toString.call(value) === '[object Date]');
-		return internals.out(isDate, 'a date', isSilent);
-	};
-
-	externals.isArray = function (isSilent) {
-		var value = internals.getValue();
-		var isArray = (Object.prototype.toString.call(value) === '[object Array]');
-		return internals.out(isArray, 'an array', isSilent);
-	};
-
-	externals.isObject = function (isSilent) {
-		var value = internals.getValue();
-		var isObject = (Object.prototype.toString.call(value) === '[object Object]');
-		return internals.out(isObject, 'an object', isSilent);
-	};
-
-	externals.isFunction = function (isSilent) {
-		var value = internals.getValue();
-		var isFunction = (Object.prototype.toString.call(value) === '[object Function]');
-		return internals.out(isFunction, 'a function', isSilent);
-	};
-
-	externals.isBoolean = function (isSilent) {
-		var value = internals.getValue();
-		var isBoolean = (Object.prototype.toString.call(value) === '[object Boolean]');
-		var isPlainBoolean = (typeof value === 'boolean');
-		var isBooleanForReal = isBoolean && isPlainBoolean;
-		return internals.out(isBooleanForReal, 'a boolean', isSilent);
-	};
-
-	externals.isString = function (isSilent) {
-		var value = internals.getValue();
-		var isString = (Object.prototype.toString.call(value) === '[object String]');
-		var isPlainString = (typeof value === 'string');
-		var isStringForReal = isString && isPlainString;
-		return internals.out(isStringForReal, 'a string', isSilent);
-	};
-
-	externals.isHexString = function (isSilent) {
-		var value = internals.getValue();
-		var isString = the(value).isString(true);
-		var isHexString = isString && (/^[0-9a-f]+$/i).test(value);
-		return internals.out(isHexString, 'a hex string', isSilent);
-	};
-
-	externals.isNull = function (isSilent) {
-		var value = internals.getValue();
-		var isNull = (value === null);
-		return internals.out(isNull, 'null', isSilent);
-	};
-
-	externals.isDefined = function (isSilent) {
-		var value = internals.getValue();
-		var isDefined = (value !== undefined);
-		return internals.out(isDefined, 'a defined value', isSilent);
-	};
-
-	externals.isNumber = function (isSilent) {
-		var value = internals.getValue();
-		var isNumber = (Object.prototype.toString.call(value) === '[object Number]');
-		var isPlainNumber = (typeof value === 'number');
-		var isFiniteNumber = isFinite(value);
-		var isNumberForReal = isNumber && isPlainNumber && isFiniteNumber;
-		return internals.out(isNumberForReal, "a number", isSilent);
-	};
-
-	internals.setValue = function (value) {
-		internals.value = value;
-	};
-
-	internals.getValue = function () {
-		return internals.value;
-	};
-
-	internals.reset = function () {
-		internals.setValue(undefined);
-	};
-
-	internals.logWarning = function (value, typeDescription) {
-		console.warn(
-			'%s Expected %s, got %s (%s)',
-			TAG,
-			typeDescription,
-			Object.prototype.toString.call(value),
-			value
-		);
-		console.trace(value);
-	};
-
-	internals.out = function (isCorrectType, typeDescription, isSilent) {
-		var value = internals.getValue();
-		if (!isCorrectType && !isSilent) {
-			internals.logWarning(value, typeDescription);
+		function hasPrototypeOf(constructor, isSilent) {
+			var value = getValue();
+			var theValue = the(value);
+			var isDefined = theValue.isDefined(true);
+			var isNotNull = !theValue.isNull(true);
+			var hasPrototypeOf = isDefined && isNotNull && Object.getPrototypeOf(value).constructor.name === constructor.name;
+			return out(hasPrototypeOf, 'value to have the prototype of ' + constructor.name, isSilent);
 		}
-		return isCorrectType;
-	};
+
+		function isInstanceOf(constructor, isSilent) {
+			var value = getValue();
+			var hasPrototypeOf = the(value).hasPrototypeOf(constructor, true);
+			var isInstanceOf = hasPrototypeOf && (value instanceof constructor);
+			return out(isInstanceOf, 'an instance of ' + constructor.name, isSilent);
+		}
+
+		function isRegExp(isSilent) {
+			var value = getValue();
+			var isRegExp = (Object.prototype.toString.call(value) === '[object RegExp]');
+			return out(isRegExp, 'a regular expression', isSilent);
+		}
+
+		function isDate(isSilent) {
+			var value = getValue();
+			var isDate = (Object.prototype.toString.call(value) === '[object Date]');
+			return out(isDate, 'a date', isSilent);
+		}
+
+		function isArray(isSilent) {
+			var value = getValue();
+			var isArray = (Object.prototype.toString.call(value) === '[object Array]');
+			return out(isArray, 'an array', isSilent);
+		}
+
+		function isObject(isSilent) {
+			var value = getValue();
+			var isObject = (Object.prototype.toString.call(value) === '[object Object]');
+			return out(isObject, 'an object', isSilent);
+		}
+
+		function isFunction(isSilent) {
+			var value = getValue();
+			var isFunction = (Object.prototype.toString.call(value) === '[object Function]');
+			return out(isFunction, 'a function', isSilent);
+		}
+
+		function isBoolean(isSilent) {
+			var value = getValue();
+			var isBoolean = (Object.prototype.toString.call(value) === '[object Boolean]');
+			var isPlainBoolean = (typeof value === 'boolean');
+			var isBooleanForReal = isBoolean && isPlainBoolean;
+			return out(isBooleanForReal, 'a boolean', isSilent);
+		}
+
+		function isString(isSilent) {
+			var value = getValue();
+			var isString = (Object.prototype.toString.call(value) === '[object String]');
+			var isPlainString = (typeof value === 'string');
+			var isStringForReal = isString && isPlainString;
+			return out(isStringForReal, 'a string', isSilent);
+		}
+
+		function isHexString(isSilent) {
+			var value = getValue();
+			var isString = the(value).isString(true);
+			var isHexString = isString && (/^[0-9a-f]+$/i).test(value);
+			return out(isHexString, 'a hex string', isSilent);
+		}
+
+		function isNull(isSilent) {
+			var value = getValue();
+			var isNull = (value === null);
+			return out(isNull, 'null', isSilent);
+		}
+
+		function isDefined(isSilent) {
+			var value = getValue();
+			var isDefined = (value !== undefined);
+			return out(isDefined, 'a defined value', isSilent);
+		}
+
+		function isNumber(isSilent) {
+			var value = getValue();
+			var isNumber = (Object.prototype.toString.call(value) === '[object Number]');
+			var isPlainNumber = (typeof value === 'number');
+			var isFiniteNumber = isFinite(value);
+			var isNumberForReal = isNumber && isPlainNumber && isFiniteNumber;
+			return out(isNumberForReal, "a number", isSilent);
+		}
+
+		function setValue(value) {
+			internals.value = value;
+		}
+
+		function getValue() {
+			return internals.value;
+		}
+
+		function logWarning(value, typeDescription) {
+			console.warn(
+				'%s Expected %s, got %s (%s)',
+				TAG,
+				typeDescription,
+				Object.prototype.toString.call(value),
+				value
+			);
+			console.trace(value);
+		}
+
+		function out(isCorrectType, typeDescription, isSilent) {
+			var value = getValue();
+			if (!isCorrectType && !isSilent) {
+				logWarning(value, typeDescription);
+			}
+			return isCorrectType;
+		}
+	}
 
 	// jshint node: true
 	try {
